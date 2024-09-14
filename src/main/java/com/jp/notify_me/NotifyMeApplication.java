03e441821d24158b5c3c7c476a24d1a60428a383
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2024-09-14 19:02:03 -0300

    first commit

---
+++ src/main/java/com/jp/notify_me/NotifyMeApplication.java
@@ -0,0 +1,13 @@
+package com.jp.notify_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/notify_me/NotifyMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.notify_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
